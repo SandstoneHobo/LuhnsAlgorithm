131353ae8665530c4281d7fc330ec98ae09432a3
MervenT <swervmerv21@gmail.com>
2025-02-07 18:13:51 -0600

    implemented the rest of the algorithm

--- scripts/luhn_algo.py
+++ scripts/luhn_algo.py
@@ -1,15 +1,23 @@
 def validate_card_number(card_number):
-    even_index_digits = []
-    odd_index_digits = []
+    check_digit = card_number[-1:]
+    new_card_number = card_number[:-1]
+    sum = 0
     try:
-        for digit in range(0, len(card_number)):
+        check_digit = int(card_number[-1:])
+        for digit in range(0, len(new_card_number)):
             if digit % 2 == 0:
-                even_index_digits.append(int(card_number[digit]))
+                sum += int(new_card_number[digit])
             else:
-                odd_index_digits.append(int(card_number[digit]) * 2)
+                sum += int(new_card_number[digit]) * 2 - 9
     except:
         print("input was not able to be parsed")
-    return (even_index_digits, odd_index_digits)
+
+    parity = (10 - (sum % 10)) % 10
+    sum += check_digit
+    if check_digit == parity and sum % 10 == 0:
+        return True
+    else:
+        return False
 
 target_card_number = "17893729974"
 
